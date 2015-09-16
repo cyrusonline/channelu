8b38129b62d00e40ea2ab1326df5580ccf851a0a
C.K. Chan <chanchikin@ied.edu.hk>
2015-09-16 12:05:40 +0800

    cannot save options and cannot popup set channel and results and final implementation 01"15

--- www/js/app.js
+++ www/js/app.js
@@ -6,7 +6,68 @@ $(document).ready(function(){
 	
 })
 
-
+//function onDeviceReady(){
+//
+//	// Check LocalStorage for channel
+//	if(localStorage.channel == null || localStorage.channel == ''){
+//		// Ask User for Channel
+//		$('#popupDialog').popup("open");
+//	} else {
+//		var channel = localStorage.getItem('channel');
+//	}
+//
+//	getPlaylist(channel);
+//
+//	$(document).on('click','#vidlist li', function(){
+//		showVideo($(this).attr('videoid'));
+//	});
+//
+//	$('#channelBtnOK').click(function(){
+//		var channel = $('#channelName').val();
+//		setChannel(channel);
+//		getPlaylist(channel);
+//	});
+//
+//	$('#saveOptions').click(function(){
+//		saveOptions();
+//	});
+//
+//	$('#clearChannel').click(function(){
+//		clearChannel();
+//	});
+//
+//	$(document).on('pageinit', '#options', function(e){
+//		var channel = localStorage.getItem('channel');
+//		var maxResults = localStorage.getItem('maxresults');
+//		$('#channelNameOptions').attr('value', channel);
+//		$('#maxResultsOptions').attr('value', maxResults);
+//	});
+//}
+//
+//function onDeviceReady(){
+//
+//	// Check LocalStorage for channel
+//	if(localStorage.channel == null || localStorage.channel == ''){
+//		// Ask User for Channel
+//		$('#popupDialog').popup("open");
+//	} else {
+//		var channel = localStorage.getItem('channel');
+//	}
+//
+//	getPlaylist(channel);
+//
+//	$(document).on('click','#vidlist li', function(){
+//		showVideo($(this).attr('videoid'));
+//	});
+//
+//	$('#channelBtnOK').click(function(){
+//		var channel = $('#channelName').val();
+//		setChannel(channel);
+//		getPlaylist(channel);
+//	});
+//
+//}
+//
 function onDeviceReady(){
 	if(localStorage.channel == null || localStorage.channel==''){
 		//Ask user for chanel
@@ -18,8 +79,25 @@ function onDeviceReady(){
 	
 	
 	$(document).on('click','#vidlist li',function(){
-		showVideo($(this).attr('videoId'));
+		showVideo($(this).attr('videoid'));
 	});
+	
+	$('#channelBtnOK').click(function(){
+		var channel = $('#channelName').val();
+		setchannel(channel);
+		getPlaylist(channel);
+
+	});
+	
+	$('#saveOptions').click(function(){
+		saveOptions();
+		
+	});
+	
+	$('clearChannel').click(function(){
+		
+	});
+	
 }
 
 
@@ -80,3 +158,28 @@ function showVideo(id){
 	var output = '<iframe width="100%" height="315" src="https://www.youtube.com/embed/'+id+'" frameborder="0" allowfullscreen></iframe>'
 	$('#ShowVideo').html(output);
 }
+
+function setchannel(channel){
+	localStorage.setItem('channel',channel);
+	console.log('channel set'+channel);
+	
+}
+
+function setMaxResults(channel){
+	localStorage.setItem('maxresults',maxResults);
+	console.log('Max Results changesd'+maxResults);
+	
+}
+
+function saveOptions(){
+	var channel = $('#channelNameOptions').val();
+	setChannel(channel);
+	car maxResults = $('#maxResultsOptions');
+	setMaxResults(maxResults);
+	$('body').pagecontainer('change','#main');
+	getPlaylist(channel);
+	
+}
+
+
+
